2bd411506e5d99c232de094cbe7d270183fe7358
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-06-02 16:20:30 -0300

    Estrutura de Dados - Vetor Genérico

---
+++ src/main/java/com/estruturadedados/vetor/Contato.java
@@ -0,0 +1,16 @@
+package com.estruturadedados.vetor;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Contato {
+	
+	private String name;
+	private String telefone;
+	private String email;
+
+}
--- src/main/java/com/estruturadedados/vetor/VetorApplication.java
+++ src/main/java/com/estruturadedados/vetor/VetorApplication.java
@@ -1,18 +1,27 @@
 package com.estruturadedados.vetor;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
 public class VetorApplication{
 
 	public static void main(String[] args) {
 		
-		Vetor vetor = new Vetor(10);
+		Contato c1 = new Contato("Jefferson", "1234-1111", "jefferson1@gmail.com");
+		Contato c2 = new Contato("Hugo", "1234-2222", "hugo1@gmail.com");
+		Contato c3 = new Contato("Luiz", "1234-3333", "luiz1@gmail.com");
+		Contato c4 = new Contato("Cruz", "1234-4444", "cruz1@gmail.com");
 		
-		vetor.adicionar("B");
-
+		VetorObject vetor = new VetorObject(7);
 		
-		System.out.println(vetor);
+		vetor.adicionar(c1);
+		vetor.adicionar(c2);
+		vetor.adicionar(c3);
+		vetor.adicionar(c4);
 		
-		
-		System.out.println(vetor);
+		System.out.println(vetor.buscar(3));
 
 	}
 
